0b62823b26ab76079cb9a944377477239833d9e5
hushamshallal <bhnjhu00@gmail.com>
2025-08-15 08:49:27 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // ❗️ استبدل 'test1' باسم مستودعك في GitHub إذا كان مختلفاً
+  base: "/englishgame1/",
+  resolve: {
+    alias: {
+      // هذا السطر يضبط المسار '@' ليشير إلى مجلد 'src'
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  }
+})
